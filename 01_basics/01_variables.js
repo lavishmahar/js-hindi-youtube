0ccf4d9b93b015e5f1e516b05460b874e35ef16d
lavishmahar <lavishmahar1@gmail.com>
2025-01-02 18:20:58 +0530

    variables
    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "lavish@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
+
+accountEmail = "lv@.com"
+ 
+
