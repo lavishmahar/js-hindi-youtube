75ccfe27efe86bddab803dc18eebc6f047adced4
lavishmahar <lavishmahar1@gmail.com>
2025-02-10 14:50:21 +0530

    Update 01_variables.js
    01_variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,10 +3,18 @@ let accountEmail = "lavish@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
 
+accountEmail= "lavish@.com"
+accountPassword = "201020"
+accountCity = "Delhi"
+let accountState;
 // accountId = 2 // not allowed
 
-console.log(accountId);
-
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
 accountEmail = "lv@.com"
  
 
